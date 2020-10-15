ba00e29a3332023479a3bda22a844af1eecf5207
Raparker88 <raparker88@gmail.com>
2020-10-15 14:37:08 -0500

    fixed bug in goldfish and overrided feed method in llama

--- llama/llama.py
+++ llama/llama.py
@@ -8,4 +8,7 @@ class Llama(Animal):
         super().__init__(name, species, food, chip_num)
         self.shift = shift 
         self.walking = True
+    
+    def feed(self):
+        print(f'on {date.today()}, {self.name} had "Rockytop" sung to it so it would eat its {self.food}')
         
